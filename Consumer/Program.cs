b4cddad588df3e1ca773ef8329b53e92fddc2258
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2019-07-10 13:19:11 -0700

    repro app

---
+++ Consumer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Library;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Connector.Connect();
+        }
+    }
+}
---
+++ Library/Connector.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace Library
+{
+    public static class Connector
+    {
+        public static void Connect()
+        {
+            var connection = new SqlConnection(@"Server=(localdb)\MSSQLLocalDB;Database=CacheTestDb;Trusted_Connection=True;");
+            connection.Open();
+        }
+    }
+}
